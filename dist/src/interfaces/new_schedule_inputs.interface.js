69a2d1f18874ef1c0b3105d697d6a2c6eabe5ac0
SMUAFIT <133557860+SMUAFIT@users.noreply.github.com>
2023-05-14 15:13:45 +0800

    Add files via upload

---
+++ dist/src/interfaces/new_schedule_inputs.interface.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=new_schedule_inputs.interface.js.map
---
+++ dist/src/interfaces/scheduling_failures.interface.d.ts
@@ -0,0 +1,11 @@
+import { Assignment, CourseConfig, CourseRun, CourseSegment } from "@prisma/client";
+export interface SchedulingIssue {
+    config?: CourseConfig;
+    reasons: string[];
+    itemsOnHold?: ItemsOnHold;
+}
+export interface ItemsOnHold {
+    courseRun: CourseRun;
+    courseSegments: CourseSegment[];
+    assignments: Assignment[];
+}
